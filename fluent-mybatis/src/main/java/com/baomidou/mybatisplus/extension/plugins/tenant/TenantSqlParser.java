3fadab9c6c4f80278360cca19d0dd2d8c5826afb
darui.wu <darui.wu@163.com>
2020-05-18 22:35:51 +0800

    mybatis-plus-extension

---
+++ fluent-mybatis/src/main/java/com/baomidou/mybatisplus/extension/plugins/tenant/TenantSqlParser.java
@@ -0,0 +1,300 @@
+/*
+ * Copyright (c) 2011-2020, baomidou (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.baomidou.mybatisplus.extension.plugins.tenant;
+
+import com.baomidou.mybatisplus.core.parser.AbstractJsqlParser;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.ExceptionUtils;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import net.sf.jsqlparser.expression.BinaryExpression;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.Parenthesis;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
+import net.sf.jsqlparser.expression.operators.relational.*;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.delete.Delete;
+import net.sf.jsqlparser.statement.insert.Insert;
+import net.sf.jsqlparser.statement.select.*;
+import net.sf.jsqlparser.statement.update.Update;
+
+import java.util.List;
+
+/**
+ * 租户 SQL 解析器（ TenantId 行级 ）
+ *
+ * @author hubin
+ * @since 2017-09-01
+ */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+public class TenantSqlParser extends AbstractJsqlParser {
+
+    private TenantHandler tenantHandler;
+
+    /**
+     * select 语句处理
+     */
+    @Override
+    public void processSelectBody(SelectBody selectBody) {
+        if (selectBody instanceof PlainSelect) {
+            processPlainSelect((PlainSelect) selectBody);
+        } else if (selectBody instanceof WithItem) {
+            WithItem withItem = (WithItem) selectBody;
+            if (withItem.getSelectBody() != null) {
+                processSelectBody(withItem.getSelectBody());
+            }
+        } else {
+            SetOperationList operationList = (SetOperationList) selectBody;
+            if (operationList.getSelects() != null && operationList.getSelects().size() > 0) {
+                operationList.getSelects().forEach(this::processSelectBody);
+            }
+        }
+    }
+
+    /**
+     * insert 语句处理
+     */
+    @Override
+    public void processInsert(Insert insert) {
+        if (tenantHandler.doTableFilter(insert.getTable().getName())) {
+            // 过滤退出执行
+            return;
+        }
+        insert.getColumns().add(new Column(tenantHandler.getTenantIdColumn()));
+        if (insert.getSelect() != null) {
+            processPlainSelect((PlainSelect) insert.getSelect().getSelectBody(), true);
+        } else if (insert.getItemsList() != null) {
+            // fixed github pull/295
+            ItemsList itemsList = insert.getItemsList();
+            if (itemsList instanceof MultiExpressionList) {
+                ((MultiExpressionList) itemsList).getExprList().forEach(el -> el.getExpressions().add(tenantHandler.getTenantId(false)));
+            } else {
+                ((ExpressionList) insert.getItemsList()).getExpressions().add(tenantHandler.getTenantId(false));
+            }
+        } else {
+            throw ExceptionUtils.mpe("Failed to process multiple-table update, please exclude the tableName or statementId");
+        }
+    }
+
+    /**
+     * update 语句处理
+     */
+    @Override
+    public void processUpdate(Update update) {
+        List<Table> tableList = update.getTables();
+        Assert.isTrue(null != tableList && tableList.size() < 2,
+            "Failed to process multiple-table update, please exclude the statementId");
+        Table table = tableList.get(0);
+        if (tenantHandler.doTableFilter(table.getName())) {
+            // 过滤退出执行
+            return;
+        }
+        update.setWhere(this.andExpression(table, update.getWhere()));
+    }
+
+    /**
+     * delete 语句处理
+     */
+    @Override
+    public void processDelete(Delete delete) {
+        if (tenantHandler.doTableFilter(delete.getTable().getName())) {
+            // 过滤退出执行
+            return;
+        }
+        delete.setWhere(this.andExpression(delete.getTable(), delete.getWhere()));
+    }
+
+    /**
+     * delete update 语句 where 处理
+     */
+    protected BinaryExpression andExpression(Table table, Expression where) {
+        //获得where条件表达式
+        EqualsTo equalsTo = new EqualsTo();
+        equalsTo.setLeftExpression(this.getAliasColumn(table));
+        equalsTo.setRightExpression(tenantHandler.getTenantId(true));
+        if (null != where) {
+            if (where instanceof OrExpression) {
+                return new AndExpression(equalsTo, new Parenthesis(where));
+            } else {
+                return new AndExpression(equalsTo, where);
+            }
+        }
+        return equalsTo;
+    }
+
+    /**
+     * 处理 PlainSelect
+     */
+    protected void processPlainSelect(PlainSelect plainSelect) {
+        processPlainSelect(plainSelect, false);
+    }
+
+    /**
+     * 处理 PlainSelect
+     *
+     * @param plainSelect ignore
+     * @param addColumn   是否添加租户列,insert into select语句中需要
+     */
+    protected void processPlainSelect(PlainSelect plainSelect, boolean addColumn) {
+        FromItem fromItem = plainSelect.getFromItem();
+        if (fromItem instanceof Table) {
+            Table fromTable = (Table) fromItem;
+            if (!this.getTenantHandler().doTableFilter(fromTable.getName())) {
+                //#1186 github
+                plainSelect.setWhere(builderExpression(plainSelect.getWhere(), fromTable));
+                if (addColumn) {
+                    plainSelect.getSelectItems().add(new SelectExpressionItem(
+                        new Column(this.getTenantHandler().getTenantIdColumn())));
+                }
+            }
+        } else {
+            processFromItem(fromItem);
+        }
+        List<Join> joins = plainSelect.getJoins();
+        if (joins != null && joins.size() > 0) {
+            joins.forEach(j -> {
+                processJoin(j);
+                processFromItem(j.getRightItem());
+            });
+        }
+    }
+
+    /**
+     * 处理子查询等
+     */
+    protected void processFromItem(FromItem fromItem) {
+        if (fromItem instanceof SubJoin) {
+            SubJoin subJoin = (SubJoin) fromItem;
+            if (subJoin.getJoinList() != null) {
+                subJoin.getJoinList().forEach(this::processJoin);
+            }
+            if (subJoin.getLeft() != null) {
+                processFromItem(subJoin.getLeft());
+            }
+        } else if (fromItem instanceof SubSelect) {
+            SubSelect subSelect = (SubSelect) fromItem;
+            if (subSelect.getSelectBody() != null) {
+                processSelectBody(subSelect.getSelectBody());
+            }
+        } else if (fromItem instanceof ValuesList) {
+            logger.debug("Perform a subquery, if you do not give us feedback");
+        } else if (fromItem instanceof LateralSubSelect) {
+            LateralSubSelect lateralSubSelect = (LateralSubSelect) fromItem;
+            if (lateralSubSelect.getSubSelect() != null) {
+                SubSelect subSelect = lateralSubSelect.getSubSelect();
+                if (subSelect.getSelectBody() != null) {
+                    processSelectBody(subSelect.getSelectBody());
+                }
+            }
+        }
+    }
+
+    /**
+     * 处理联接语句
+     */
+    protected void processJoin(Join join) {
+        if (join.getRightItem() instanceof Table) {
+            Table fromTable = (Table) join.getRightItem();
+            if (this.tenantHandler.doTableFilter(fromTable.getName())) {
+                // 过滤退出执行
+                return;
+            }
+            join.setOnExpression(builderExpression(join.getOnExpression(), fromTable));
+        }
+    }
+
+    /**
+     * 处理条件:
+     * 支持 getTenantHandler().getTenantId()是一个完整的表达式：tenant in (1,2)
+     * 默认tenantId的表达式： LongValue(1)这种依旧支持
+     */
+    protected Expression builderExpression(Expression currentExpression, Table table) {
+        final Expression tenantExpression = this.getTenantHandler().getTenantId(false);
+        Expression appendExpression;
+        if (!(tenantExpression instanceof SupportsOldOracleJoinSyntax)) {
+            appendExpression = new EqualsTo();
+            ((EqualsTo) appendExpression).setLeftExpression(this.getAliasColumn(table));
+            ((EqualsTo) appendExpression).setRightExpression(tenantExpression);
+        } else {
+            appendExpression = processTableAlias4CustomizedTenantIdExpression(tenantExpression, table);
+        }
+        if (currentExpression == null) {
+            return appendExpression;
+        }
+        if (currentExpression instanceof BinaryExpression) {
+            BinaryExpression binaryExpression = (BinaryExpression) currentExpression;
+            if (binaryExpression.getLeftExpression() instanceof FromItem) {
+                processFromItem((FromItem) binaryExpression.getLeftExpression());
+            }
+            if (binaryExpression.getRightExpression() instanceof FromItem) {
+                processFromItem((FromItem) binaryExpression.getRightExpression());
+            }
+        } else if (currentExpression instanceof InExpression) {
+            InExpression inExp = (InExpression) currentExpression;
+            ItemsList rightItems = inExp.getRightItemsList();
+            if (rightItems instanceof SubSelect) {
+                processSelectBody(((SubSelect) rightItems).getSelectBody());
+            }
+        }
+        if (currentExpression instanceof OrExpression) {
+            return new AndExpression(new Parenthesis(currentExpression), appendExpression);
+        } else {
+            return new AndExpression(currentExpression, appendExpression);
+        }
+    }
+
+    /**
+     * 目前: 针对自定义的tenantId的条件表达式[tenant_id in (1,2,3)]，无法处理多租户的字段加上表别名
+     * select a.id, b.name
+     * from a
+     * join b on b.aid = a.id and [b.]tenant_id in (1,2) --别名[b.]无法加上 TODO
+     *
+     * @param expression
+     * @param table
+     * @return 加上别名的多租户字段表达式
+     */
+    protected Expression processTableAlias4CustomizedTenantIdExpression(Expression expression, Table table) {
+        //cannot add table alias for customized tenantId expression,
+        // when tables including tenantId at the join table poistion
+        return expression;
+    }
+
+    /**
+     * 租户字段别名设置
+     * <p>tableName.tenantId 或 tableAlias.tenantId</p>
+     *
+     * @param table 表对象
+     * @return 字段
+     */
+    protected Column getAliasColumn(Table table) {
+        StringBuilder column = new StringBuilder();
+        if (null == table.getAlias()) {
+            column.append(table.getName());
+        } else {
+            column.append(table.getAlias().getName());
+        }
+        column.append(StringPool.DOT);
+        column.append(tenantHandler.getTenantIdColumn());
+        return new Column(column.toString());
+    }
+}
---
+++ fluent-mybatis/src/main/java/com/baomidou/mybatisplus/extension/toolkit/JdbcUtils.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2011-2020, baomidou (jobob@qq.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.baomidou.mybatisplus.extension.toolkit;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+
+/**
+ * JDBC 工具类
+ *
+ * @author nieqiurong
+ * @since 2016-12-05
+ */
+public class JdbcUtils {
+
+    private static final Log logger = LogFactory.getLog(JdbcUtils.class);
+
+    /**
+     * 根据连接地址判断数据库类型
+     *
+     * @param jdbcUrl 连接地址
+     * @return ignore
+     */
+    public static DbType getDbType(String jdbcUrl) {
+        Assert.isFalse(StringUtils.isEmpty(jdbcUrl), "Error: The jdbcUrl is Null, Cannot read database type");
+        if (jdbcUrl.contains(":mysql:") || jdbcUrl.contains(":cobar:")) {
+            return DbType.MYSQL;
+        } else if (jdbcUrl.contains(":mariadb:")) {
+            return DbType.MARIADB;
+        } else if (jdbcUrl.contains(":oracle:")) {
+            return DbType.ORACLE;
+        } else if (jdbcUrl.contains(":sqlserver:") || jdbcUrl.contains(":microsoft:")) {
+            return DbType.SQL_SERVER2005;
+        } else if (jdbcUrl.contains(":sqlserver2012:")) {
+            return DbType.SQL_SERVER;
+        } else if (jdbcUrl.contains(":postgresql:")) {
+            return DbType.POSTGRE_SQL;
+        } else if (jdbcUrl.contains(":hsqldb:")) {
+            return DbType.HSQL;
+        } else if (jdbcUrl.contains(":db2:")) {
+            return DbType.DB2;
+        } else if (jdbcUrl.contains(":sqlite:")) {
+            return DbType.SQLITE;
+        } else if (jdbcUrl.contains(":h2:")) {
+            return DbType.H2;
+        } else if (jdbcUrl.contains(":dm:")) {
+            return DbType.DM;
+        } else {
+            logger.warn("The jdbcUrl is " + jdbcUrl + ", Mybatis Plus Cannot Read Database type or The Database's Not Supported!");
+            return DbType.OTHER;
+        }
+    }
+}
