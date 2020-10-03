24b100548d340216aa78dd487bcf4b42b789cc1b
darui.wu <darui.wu@163.com>
2020-10-03 23:06:41 +0800

    fix

--- fluent-mybatis-generator/src/main/java/cn/org/atool/fluent/mybatis/generator/EntityGenerator.java
+++ fluent-mybatis-generator/src/main/java/cn/org/atool/fluent/mybatis/generator/EntityGenerator.java
@@ -8,10 +8,12 @@ import org.test4j.generator.mybatis.DataMapGenerator;
 import org.test4j.generator.mybatis.config.IGlobalConfig;
 import org.test4j.generator.mybatis.template.BaseTemplate;
 import org.test4j.generator.mybatis.template.DataMapTemplateList;
+import org.test4j.generator.mybatis.template.summary.SummaryTemplate;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 数据库表列表
@@ -23,6 +25,19 @@ public class EntityGenerator extends DataMapGenerator {
     private EntityGenerator() {
     }
 
+    private boolean withTest = false;
+
+    /**
+     * 生成fluent mybatis文件
+     *
+     * @return
+     */
+    public static IGlobalConfig buildWithTest() {
+        EntityGenerator generator = new EntityGenerator();
+        generator.withTest = true;
+        return generator;
+    }
+
     /**
      * 生成fluent mybatis文件
      *
@@ -32,19 +47,34 @@ public class EntityGenerator extends DataMapGenerator {
         return new EntityGenerator();
     }
 
-    static List<BaseTemplate> list = new ArrayList<>();
+    private List<BaseTemplate> list = new ArrayList<>();
 
-    static {
-        list.addAll(Arrays.asList(
-            new EntityTemplate(),
-            new DaoIntfTemplate(),
-            new DaoImplTemplate()
-        ));
-        list.addAll(DataMapTemplateList.datamap_list);
+    @Override
+    protected synchronized List<BaseTemplate> getAllTemplates() {
+        if (list.size() == 0) {
+            list.add(new EntityTemplate());
+            list.add(new DaoIntfTemplate());
+            list.add(new DaoImplTemplate());
+            if (withTest) {
+                list.addAll(DataMapTemplateList.datamap_list);
+            }
+        }
+        return list;
     }
 
     @Override
-    protected List<BaseTemplate> getAllTemplates() {
-        return list;
+    protected void generateSummary(List<Map<String, Object>> allContext) {
+        Map<String, Object> wrapper = new HashMap<>();
+        {
+            wrapper.put("configs", allContext);
+            wrapper.put("basePackage", this.globalConfig.getBasePackage());
+        }
+        if (!withTest) {
+            return;
+        }
+        for (SummaryTemplate summary : DataMapTemplateList.summaries) {
+            summary.setGlobalConfig(this.globalConfig);
+            templateEngine.output(summary.getTemplateId(), wrapper, summary.getFilePath());
+        }
     }
 }
