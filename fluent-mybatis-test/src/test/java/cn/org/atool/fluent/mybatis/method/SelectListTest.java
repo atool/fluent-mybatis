e0dfeba6f24589a4a7afb59a2d9d217c2ab2cfbb
LinFan <linfan.china@gmail.com>
2020-11-06 16:04:19 +0800

    add test case for number and date type query

--- fluent-mybatis-test/src/test/java/cn/org/atool/fluent/mybatis/method/SelectListTest.java
+++ fluent-mybatis-test/src/test/java/cn/org/atool/fluent/mybatis/method/SelectListTest.java
@@ -8,6 +8,7 @@ import cn.org.atool.fluent.mybatis.test.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -77,5 +78,20 @@ public class SelectListTest extends BaseTest {
         List<StudentEntity> users = mapper.listEntity(query);
         db.sqlList().wantFirstSql().start("SELECT").end("FROM t_student WHERE user_name = ? LIMIT ?, ?");
         db.sqlList().wantFirstPara().eq(new Object[]{"u2", 2, 3});
+    }
+
+    @Test
+    public void test_selectList_withTimeRange() throws Exception {
+        db.table(ATM.Table.student).clean()
+            .insert(ATM.DataMap.student.initTable(4)
+                .id.values(23, 24, 25, 26)
+                .gmtCreated.values(new Date(1604140000000L), new Date(1604150000000L), new Date(1604160000000L), new Date(1604170000000L))
+            );
+        StudentQuery query = new StudentQuery()
+            .where.gmtCreated().gt(new Date(1604140000000L))
+            .and.gmtCreated().le(new Date(1604170000000L)).end();
+        List<StudentEntity> users = mapper.listEntity(query);
+        db.sqlList().wantFirstSql().start("SELECT").end("FROM t_student WHERE gmt_created > ? AND gmt_created <= ?");
+        want.list(users).eqDataMap(ATM.DataMap.student.entity(3));
     }
 }
--- fluent-mybatis-test/src/test/java/cn/org/atool/fluent/mybatis/test/basedao/SelectByMapTest.java
+++ fluent-mybatis-test/src/test/java/cn/org/atool/fluent/mybatis/test/basedao/SelectByMapTest.java
@@ -8,6 +8,7 @@ import cn.org.atool.fluent.mybatis.test.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -20,20 +21,55 @@ public class SelectByMapTest extends BaseTest {
     private StudentExtDao dao;
 
     @Test
-    public void test_selectByMap() throws Exception {
+    public void test_selectByMap_withStringField() throws Exception {
         ATM.DataMap.student.initTable(10)
             .userName.values(DataGenerator.increase("username_%d"))
             .env.values("test_env")
             .cleanAndInsert();
 
-        List<StudentEntity> users = dao.selectByMap(new HashMap<String, Object>() {
-            {
-                this.put(StudentMapping.userName.column, "username_4");
-            }
-        });
+        List<StudentEntity> users = dao.selectByMap(new HashMap<String, Object>() {{
+            put(StudentMapping.userName.column, "username_4");
+        }});
         db.sqlList().wantFirstSql().start("SELECT")
             .end("FROM t_student WHERE is_deleted = ? AND env = ? AND user_name = ?");
         want.list(users).eqDataMap(ATM.DataMap.student.entity(1)
             .userName.values("username_4"));
+    }
+
+    @Test
+    public void test_selectByMap_withNumberField() throws Exception {
+        ATM.DataMap.student.initTable(3)
+            .age.values(DataGenerator.increase(20, 1))
+            .env.values("test_env")
+            .cleanAndInsert();
+
+        List<StudentEntity> students = dao.selectByMap(new HashMap<String, Object>() {{
+            put(StudentMapping.age.column, 21);
+        }});
+        db.sqlList().wantFirstSql().start("SELECT")
+            .end("FROM t_student WHERE is_deleted = ? AND env = ? AND age = ?");
+        want.object(students)
+            .eqMap(ATM.DataMap.student.entity()
+                .age.values(21)
+            );
+    }
+
+    @Test
+    public void test_selectByMap_withDateField() throws Exception {
+        Date date = new Date(1604160000000L);
+        ATM.DataMap.student.initTable(1)
+            .gmtModified.values(date)
+            .env.values("test_env")
+            .cleanAndInsert();
+
+        List<StudentEntity> students = dao.selectByMap(new HashMap<String, Object>() {{
+            put(StudentMapping.gmtModified.column, date);
+        }});
+        db.sqlList().wantFirstSql().start("SELECT")
+            .end("FROM t_student WHERE is_deleted = ? AND env = ? AND gmt_modified = ?");
+        want.object(students)
+            .eqMap(ATM.DataMap.student.entity()
+                .gmtModified.values(date)
+            );
     }
 }
