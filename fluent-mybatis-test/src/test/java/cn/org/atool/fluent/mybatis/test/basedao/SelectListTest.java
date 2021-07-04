d0545ecbcf704e45499d7e732cec4a334e4b4cdb
darui.wu <darui.wu@163.com>
2021-07-04 21:18:26 +0800

    add test

--- fluent-mybatis-test/src/test/java/cn/org/atool/fluent/mybatis/test/basedao/SelectListTest.java
+++ fluent-mybatis-test/src/test/java/cn/org/atool/fluent/mybatis/test/basedao/SelectListTest.java
@@ -3,6 +3,9 @@ package cn.org.atool.fluent.mybatis.test.basedao;
 import cn.org.atool.fluent.mybatis.customize.StudentExtDao;
 import cn.org.atool.fluent.mybatis.generate.ATM;
 import cn.org.atool.fluent.mybatis.generate.entity.StudentEntity;
+import cn.org.atool.fluent.mybatis.generate.mapper.StudentMapper;
+import cn.org.atool.fluent.mybatis.generate.wrapper.StudentQuery;
+import cn.org.atool.fluent.mybatis.generate.wrapper.StudentUpdate;
 import cn.org.atool.fluent.mybatis.test.BaseTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +21,9 @@ public class SelectListTest extends BaseTest {
     @Autowired
     private StudentExtDao dao;
 
+    @Autowired
+    private StudentMapper mapper;
+
     @Test
     public void test_selectList() throws Exception {
         ATM.dataMap.student.initTable(10)
@@ -30,5 +36,81 @@ public class SelectListTest extends BaseTest {
             .id.values(3, 6, 7)
             .userName.values("username_3", "username_6", "username_7")
         );
+    }
+
+    @Test
+    public void test_QueryDirectly() throws Exception {
+        ATM.dataMap.student.initTable(10)
+            .userName.values(DataGenerator.increase("username_%d"))
+            .env.values("test_env")
+            .cleanAndInsert();
+
+        List<StudentEntity> users = new StudentQuery()
+            .where.id().in(new long[]{3L, 6L, 7L}).end()
+            .of(mapper).listEntity();
+
+        want.list(users).eqDataMap(ATM.dataMap.student.entity(3)
+            .id.values(3, 6, 7)
+            .userName.values("username_3", "username_6", "username_7")
+        );
+    }
+
+    @Test
+    public void test_QueryDirectly2() throws Exception {
+        ATM.dataMap.student.initTable(10)
+            .userName.values(DataGenerator.increase("username_%d"))
+            .env.values("test_env")
+            .cleanAndInsert();
+
+        List<StudentEntity> users = new StudentQuery()
+            .where.id().in(new long[]{3L, 6L, 7L}).end()
+            .to().listEntity();
+
+        want.list(users).eqDataMap(ATM.dataMap.student.entity(3)
+            .id.values(3, 6, 7)
+            .userName.values("username_3", "username_6", "username_7")
+        );
+    }
+
+    @Test
+    public void test_UpdateDirectly() throws Exception {
+        ATM.dataMap.student.initTable(3)
+            .id.values(1, 2, 3)
+            .userName.values("user_1", "user_2", "user_3")
+            .env.values("test_env")
+            .cleanAndInsert();
+
+        new StudentUpdate()
+            .set.userName().is("test").end()
+            .where.id().eq(2L)
+            .and.env().eq("test_env").end()
+            .of(mapper).updateBy();
+
+        ATM.dataMap.student.table(3)
+            .id.values(1, 2, 3)
+            .userName.values("user_1", "test", "user_3")
+            .env.values("test_env")
+            .eqTable();
+    }
+
+    @Test
+    public void test_UpdateDirectly2() throws Exception {
+        ATM.dataMap.student.initTable(3)
+            .id.values(1, 2, 3)
+            .userName.values("user_1", "user_2", "user_3")
+            .env.values("test_env")
+            .cleanAndInsert();
+
+        new StudentUpdate()
+            .set.userName().is("test").end()
+            .where.id().eq(2L)
+            .and.env().eq("test_env").end()
+            .to().updateBy();
+
+        ATM.dataMap.student.table(3)
+            .id.values(1, 2, 3)
+            .userName.values("user_1", "test", "user_3")
+            .env.values("test_env")
+            .eqTable();
     }
 }
