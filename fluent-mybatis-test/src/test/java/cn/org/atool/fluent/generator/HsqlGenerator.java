b8612e420e9a65d2af8f6466e97502f83a323601
darui.wu <darui.wu@163.com>
2021-08-08 19:57:37 +0800

    db-feature

---
+++ fluent-mybatis-test/src/test/java/cn/org/atool/fluent/generator/HsqlGenerator.java
@@ -0,0 +1,44 @@
+package cn.org.atool.fluent.generator;
+
+import cn.org.atool.generator.FileGenerator;
+import cn.org.atool.generator.annotation.Table;
+import cn.org.atool.generator.annotation.Tables;
+import cn.org.atool.generator.database.DbTypeOfGenerator;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.test4j.module.database.proxy.DataSourceCreator;
+import org.test4j.module.database.script.DataSourceType;
+
+import javax.sql.DataSource;
+
+import static cn.org.atool.fluent.generator.FluentMyBatisGeneratorMain.BasePack;
+
+public class HsqlGenerator {
+    DataSource sqliteDataSource() {
+        return DataSourceCreator.create("dataSource", DataSourceType.HSQLDB,
+            "org.sqlite.JDBC",
+            "jdbc:sqlite:" + System.getProperty("user.dir") + "/db/sqlite_test",
+            "", ""
+        );
+    }
+
+    @Disabled
+    @Test
+    void generate() {
+        FileGenerator.build(sqliteDataSource(), A.class);
+    }
+
+    @Tables(
+        dbType = DbTypeOfGenerator.SQLITE,
+        driver = "org.sqlite.JDBC",
+        url = "jdbc:sqlite:db/sqlite_test",
+        username = "", password = "",
+        basePack = BasePack, schema = "",
+        tables = {
+            @Table(value = {"student"})
+        },
+        /* 只是测试需要, 正式项目请生产文件到对应的src目录下 */
+        srcDir = "target/sqlite/entity", testDir = "target/sqlite/test", daoDir = "target/sqlite/dao")
+    static class A {
+    }
+}
