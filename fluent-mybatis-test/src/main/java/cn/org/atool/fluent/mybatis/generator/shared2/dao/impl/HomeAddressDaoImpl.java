c150d16c60b96c3b10de36634c8381cc3ffdead6
darui.wu <darui.wu@163.com>
2021-11-24 22:25:12 +0800

    comments

--- fluent-mybatis-test/src/main/java/cn/org/atool/fluent/mybatis/generator/shared2/dao/impl/HomeAddressDaoImpl.java
+++ fluent-mybatis-test/src/main/java/cn/org/atool/fluent/mybatis/generator/shared2/dao/impl/HomeAddressDaoImpl.java
@@ -1,16 +1,8 @@
 package cn.org.atool.fluent.mybatis.generator.shared2.dao.impl;
 
-import cn.org.atool.fluent.mybatis.generator.shared2.dao.base.HomeAddressBaseDao;
-import cn.org.atool.fluent.mybatis.generator.shared2.dao.intf.HomeAddressDao;
-import org.springframework.stereotype.Repository;
-
 /**
- * HomeAddressDaoImpl: 数据操作接口实现
- *
- * 这只是一个减少手工创建的模板文件
- * 可以任意添加方法和实现, 更改作者和重定义类名
- * <p/>@author Powered By Fluent Mybatis
+ * 占位, 避免重新生成
  */
-@Repository
-public class HomeAddressDaoImpl extends HomeAddressBaseDao implements HomeAddressDao {
+@SuppressWarnings({"unused"})
+public class HomeAddressDaoImpl {
 }
