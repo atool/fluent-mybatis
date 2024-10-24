97abab121401064efa7bf128eccccd26a96edc04
daruiwu <wu.darui@boredflow.com>
2024-10-24 16:00:33 +0800

    PgKit

--- fluent-mybatis/src/main/java/cn/org/atool/fluent/mybatis/kits/ApplyKit.java
+++ fluent-mybatis/src/main/java/cn/org/atool/fluent/mybatis/kits/ApplyKit.java
@@ -3,11 +3,11 @@ package cn.org.atool.fluent.mybatis.kits;
 import cn.org.atool.fluent.mybatis.base.model.FieldMapping;
 
 @SuppressWarnings("rawtypes")
-public interface ApplyKit {
+public class ApplyKit {
     /**
      * field = field + ?
      */
-    static ApplyFunc plus(FieldMapping field, Object value) {
+    public static ApplyFunc plus(FieldMapping field, Object value) {
         return new ApplyFunc(field.column + " + ?").args(value);
     }
 }
--- fluent-mybatis/src/main/java/cn/org/atool/fluent/mybatis/kits/PgKit.java
+++ fluent-mybatis/src/main/java/cn/org/atool/fluent/mybatis/kits/PgKit.java
@@ -7,40 +7,40 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @SuppressWarnings({"rawtypes", "unused"})
-public interface PgKit {
-    static <T> String joining(T[] fields, Function<T, String> mapping) {
+public class PgKit extends ApplyKit {
+    public static <T> String joining(T[] fields, Function<T, String> mapping) {
         return Arrays.stream(fields)
             .map(mapping)
             .collect(Collectors.joining(", "));
     }
 
-    static String onConflictDoNothing(String statement, FieldMapping... fields) {
+    public static String onConflictDoNothing(String statement, FieldMapping... fields) {
         String uIndex = joining(fields, f -> f.column);
         return String.format("%s ON CONFLICT(%s) DO NOTHING", statement, uIndex);
     }
 
-    static String onConflictDoNothing(String statement, String... fields) {
+    public static String onConflictDoNothing(String statement, String... fields) {
         String uIndex = joining(fields, f -> f);
         return String.format("%s ON CONFLICT(%s) DO NOTHING", statement, uIndex);
     }
 
-    static String onConflictDoUpdate(String statement, FieldMapping[] fields, String updateBy) {
+    public static String onConflictDoUpdate(String statement, FieldMapping[] fields, String updateBy) {
         String uIndex = joining(fields, f -> f.column);
         return String.format("%s ON CONFLICT(%s) DO UPDATE SET %s", statement, uIndex, updateBy);
     }
 
-    static String onConflictDoUpdate(String statement, String[] fields, String updateBy) {
+    public static String onConflictDoUpdate(String statement, String[] fields, String updateBy) {
         String uIndex = joining(fields, f -> f);
         return String.format("%s ON CONFLICT(%s) DO UPDATE SET %s", statement, uIndex, updateBy);
     }
 
-    static String onConflictDoUpdate(String statement, FieldMapping[] fields, FieldMapping... updates) {
+    public static String onConflictDoUpdate(String statement, FieldMapping[] fields, FieldMapping... updates) {
         String uIndex = joining(fields, f -> f.column);
         String updateBy = joining(updates, f -> String.format("%s = excluded.%s", f.column, f.column));
         return String.format("%s ON CONFLICT(%s) DO UPDATE SET %s", statement, uIndex, updateBy);
     }
 
-    static String onConflictDoUpdate(String statement, String[] fields, String[] updates) {
+    public static String onConflictDoUpdate(String statement, String[] fields, String[] updates) {
         String uIndex = String.join(", ", fields);
         String updateBy = joining(updates, f -> String.format("%s = excluded.%s", f, f));
         return String.format("%s ON CONFLICT(%s) DO UPDATE SET %s", statement, uIndex, updateBy);
